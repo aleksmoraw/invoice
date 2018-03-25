48f6be61de4799f2c2870dbefa595d19f02c5034
aleksandramorawska <aleksandra_morawska@onet.pl>
2018-03-25 20:53:01 +0200

    added case where file does not exist

--- src/main/java/myApp/DataSave.java
+++ src/main/java/myApp/DataSave.java
@@ -1,8 +1,10 @@
 package myApp;
 
 import org.odftoolkit.simple.SpreadsheetDocument;
+import org.odftoolkit.simple.table.Cell;
 import org.odftoolkit.simple.table.Table;
 
+import java.io.File;
 import java.util.List;
 
 public class DataSave {
@@ -12,13 +14,21 @@ public class DataSave {
     public void saveFile(String filePath, List<String> invoiceDetailsList) {
 
         try {
-            SpreadsheetDocument loadedExistedFile = SpreadsheetDocument.loadDocument(filePath);
-            Table table = loadedExistedFile.getSheetByIndex(0);
+            File file = new File(filePath);
+            SpreadsheetDocument spreadsheetDocument;
 
-            loadHeaders(table);
-            loadInvoiceDetails(table, invoiceDetailsList);
+            if(file.exists()) {
+                spreadsheetDocument = SpreadsheetDocument.loadDocument(filePath);
+                Table table = spreadsheetDocument.getSheetByIndex(0);
+                loadInvoiceDetails(table, invoiceDetailsList);
+            } else {
+                spreadsheetDocument = SpreadsheetDocument.newSpreadsheetDocument();
+                Table table = spreadsheetDocument.getSheetByIndex(0);
+                loadHeaders(table);
+                loadInvoiceDetails(table, invoiceDetailsList);
+            }
 
-            loadedExistedFile.save(filePath);
+            spreadsheetDocument.save(filePath);
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -30,7 +40,9 @@ public class DataSave {
         int columnCount = 0;
 
         for (String header : FILE_HEADER) {
-            table.getCellByPosition(columnCount++, rowCount).setStringValue(header);
+            Cell cell = table.getCellByPosition(columnCount++, rowCount);
+            cell.setStringValue(header);
+            cell.setCellBackgroundColor("#A9A9A9");
         }
     }
 
